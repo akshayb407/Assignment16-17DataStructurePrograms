9c1c7484c47bd0b164277b78fe866308312cfa36
akshayb407 <akshayb407@gmail.com>
2022-08-25 13:57:06 +0530

    UC3_BalancedParentheses added

---
+++ DataStructurePrograms/BalancedParentheses.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataStructurePrograms
+{
+    public class BalancedParentheses
+    {
+        public bool isBalancedParentheses(string filePath)
+        {
+            string[] listofChars = File.ReadAllText(filePath).Split(" ");
+            if (listofChars[0] == ")")
+            {
+                Console.WriteLine("Given equation is of unbalnced parentheses.");
+                return false;
+            }
+            MyStack<string> stack = new MyStack<string>();
+            foreach (string cha in listofChars)
+            {
+                if (cha == "(")
+                {
+                    stack.Push(cha);
+                }
+                if (stack.top == null && cha == ")")
+                {
+                    Console.WriteLine("Given equation is of unbalnced parentheses.");
+                    return false;
+                }
+                if (cha == ")")
+                {
+                    stack.Pop();
+                }
+            }
+            if (stack.isEmpty())
+            {
+                Console.WriteLine("Given equation is of balanecd parentheses.");
+            }
+            else
+            {
+                Console.WriteLine("Given equation is of unbalnced parentheses.");
+            }
+            return stack.isEmpty();
+        }
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -12,11 +12,12 @@ namespace DataStructurePrograms
 
             string inputFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\InputFile.txt";
             string outputFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\OutputFile.txt";
+            string equationFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\Equation.txt";
             bool isRun = true;
             while (isRun)
             {
                 Console.WriteLine("Select and enter program number:" +
-                    "(1. Unordered List, 2. Ordered List");
+                    "(1. Unordered List, 2. Ordered List, 3. Balanced Parentheses");
                 int programOption = Convert.ToInt32(Console.ReadLine());
                 switch (programOption)
                 {
@@ -25,6 +26,9 @@ namespace DataStructurePrograms
                         break;
                     case 2:
                         new OrderedList().OrderedListOperations(inputFilePath, outputFilePath);
+                        break;
+                    case 3:
+                        new BalancedParentheses().isBalancedParentheses(equationFilePath);
                         break;
                     default:
                         isRun = !isRun;
