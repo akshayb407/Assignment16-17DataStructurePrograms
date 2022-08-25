f3880d73de731a30c29e68a70ff99d4648b60d20
akshayb407 <akshayb407@gmail.com>
2022-08-25 14:11:29 +0530

    UC4_Banking_Cash_Counter added

---
+++ DataStructurePrograms/MyQueue.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DataStructurePrograms
+{
+    public class MyQueue<T>
+    {
+        public Node<T> head = null;
+        public void Enqueue(T data)
+        {
+            Node<T> node = new Node<T>(data);
+            if (head == null)
+            {
+                head = node;
+            }
+            else
+            {
+                Node<T> temp = head;
+                while (temp.nextNode != null)
+                {
+                    temp = temp.nextNode;
+                }
+                temp.nextNode = node;
+            }
+        }
+        public void DisplayQueue()
+        {
+            Node<T> temp = this.head;
+            if (temp == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            while (temp != null)
+            {
+                Console.WriteLine(temp.data + " ");
+                temp = temp.nextNode;
+            }
+        }
+        public void Dequeue()
+        {
+            if (head == null)
+            {
+                Console.WriteLine("Queue is Empty");
+                return;
+            }
+            head = head.nextNode;
+        }
+        public void EmptyQueue()
+        {
+            while (head != null)
+            {
+                head = head.nextNode;
+            }
+        }
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -17,7 +17,7 @@ namespace DataStructurePrograms
             while (isRun)
             {
                 Console.WriteLine("Select and enter program number:" +
-                    "(1. Unordered List, 2. Ordered List, 3. Balanced Parentheses");
+                    "(1. Unordered List, 2. Ordered List, 3. Balanced Parentheses, 4. Bank Cash Counter");
                 int programOption = Convert.ToInt32(Console.ReadLine());
                 switch (programOption)
                 {
@@ -30,11 +30,47 @@ namespace DataStructurePrograms
                     case 3:
                         new BalancedParentheses().isBalancedParentheses(equationFilePath);
                         break;
+                    case 4:
+
+                        MyQueue<string> customerQueue = new MyQueue<string>();
+                        BankCashCounter cashCounter = new BankCashCounter();
+                        cashCounter.AddPeopleToQueue(customerQueue);
+                        while (customerQueue.head != null)
+                        {
+                            string customer = customerQueue.head.data;
+                            string customerName = customer.Split("-")[0];
+                            int customerBalance = Convert.ToInt32(customer.Split("-")[1]);
+                            bool isDoNextTransaction = true;
+                            while (isDoNextTransaction)
+                            {
+                                Console.WriteLine($"{customerName},\nSelect an option of transaction(1. Withdraw, 2. Deposit, 3. Check balance, 4. Exit): ");
+                                int transactionOption = Convert.ToInt32(Console.ReadLine());
+                                switch (transactionOption)
+                                {
+                                    case 1:
+                                        cashCounter.WithDraw(customerName, customerBalance);
+                                        break;
+                                    case 2:
+                                        cashCounter.Deposit(customerName, customerBalance);
+                                        break;
+                                    case 3:
+                                        cashCounter.BalanceCheck(customerName, customerBalance);
+                                        break;
+                                    default:
+                                        customerQueue.Dequeue();
+                                        isDoNextTransaction = !isDoNextTransaction;
+                                        break;
+                                }
+                            }
+
+                        }
+                        break;
                     default:
                         isRun = !isRun;
                         break;
                 }
             }
         }
+        
     }
 }
