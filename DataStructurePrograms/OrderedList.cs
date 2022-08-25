85ef7fd8e4a8d9a124de8200a40aac03594d861b
akshayb407 <akshayb407@gmail.com>
2022-08-25 13:36:01 +0530

    UC2_Ordered_List added

---
+++ DataStructurePrograms/OrderedList.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace DataStructurePrograms
+{
+    public class OrderedList
+    {
+        public void OrderedListOperations(string inputFilePath, string outputFilePath)
+        {
+            string[] wordsList = File.ReadAllText(inputFilePath).Split(",");
+            MyLinkedList<string> listOfWords = new MyLinkedList<string>();
+            foreach (string word in wordsList)
+            {
+                listOfWords.AddNode(word);
+            }
+            bool isEdit = true;
+            while (isEdit)
+            {
+                Console.WriteLine("Enter 1 to edit data, 2 to exit");
+                int editOption = Convert.ToInt32(Console.ReadLine());
+                switch (editOption)
+                {
+                    case 1:
+                        Console.WriteLine("Enter word to search: ");
+                        string searchWord = Console.ReadLine();
+                        int position = listOfWords.Search(searchWord);
+                        if (position >= 0)
+                        {
+                            Console.WriteLine("Deleting the searched word...");
+                            listOfWords.DeleteANumber(searchWord);
+                        }
+                        else
+                        {
+                            Console.WriteLine("Adding word to list");
+                            listOfWords.AddNode(searchWord);
+                        }
+                        break;
+                    default:
+                        isEdit = !isEdit;
+                        break;
+                }
+            }
+            MyLinkedList<string> sortedList = listOfWords.SortList();
+            List<string> outputList = sortedList.LinkedListToList();
+            string a = "";
+            foreach (string word in outputList)
+            {
+                a = a + "," + word;
+            }
+            string output = a.Remove(0, 1);
+            File.WriteAllText(outputFilePath, output);
+        }
+
+    }
+}
--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -12,7 +12,25 @@ namespace DataStructurePrograms
 
             string inputFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\InputFile.txt";
             string outputFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\OutputFile.txt";
-            new UnOrderedList().UnorderListOperations(inputFilePath, outputFilePath);
+            bool isRun = true;
+            while (isRun)
+            {
+                Console.WriteLine("Select and enter program number:" +
+                    "(1. Unordered List, 2. Ordered List");
+                int programOption = Convert.ToInt32(Console.ReadLine());
+                switch (programOption)
+                {
+                    case 1:
+                        new UnOrderedList().UnorderListOperations(inputFilePath, outputFilePath);
+                        break;
+                    case 2:
+                        new OrderedList().OrderedListOperations(inputFilePath, outputFilePath);
+                        break;
+                    default:
+                        isRun = !isRun;
+                        break;
+                }
+            }
         }
     }
 }
