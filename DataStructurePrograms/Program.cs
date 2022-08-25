88aa0355e0bd1583a2dfb2d6c94a73fcaf9be7cd
akshayb407 <akshayb407@gmail.com>
2022-08-25 13:23:54 +0530

    UC1_UnOrdered_List added

--- DataStructurePrograms/Program.cs
+++ DataStructurePrograms/Program.cs
@@ -1,12 +1,18 @@
 ﻿using System;
+using System.Collections.Generic;
+
 
 namespace DataStructurePrograms
 {
-    internal class Program
+    public class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to Data Structure Programs!");
+
+            string inputFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\InputFile.txt";
+            string outputFilePath = @"H:\All Asignment\Assignment16&17\DataStructures\DataStructurePrograms\DataStructurePrograms\OutputFile.txt";
+            new UnOrderedList().UnorderListOperations(inputFilePath, outputFilePath);
         }
     }
 }
